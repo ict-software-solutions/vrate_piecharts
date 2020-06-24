614fb0b0f22059061438d4765b4b333798eefd12
Godwin Stanislaus <godwin.stanislaus@archents.com>
2020-06-24 22:45:43 +0100

    node server added

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 var express = require('express')
-
 var app = express();
-
 app.use(express.static('.'));
-app.get('/', (req, res) => {
-    res.send(' yes, am alive!')
-})
-
 app.listen( 3003, () => console.log('application is listeninsg on port 3000') )
